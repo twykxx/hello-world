8db26eeb3423af75ce26f75af521ef40e3216426
twykxx <julian.gobert@gmail.com>
2016-09-05 15:53:35 +0200

    Update tag :20: (reconfirmations)

--- cls_migration_mt300.sh
+++ cls_migration_mt300.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Paramètres
-input=`pwd`					              # Path des fichiers input (pwd = répertoire courant)
-output="/home/gobertj1a/tmp"			# Path pour les fichiers/répertoires output
-output_prefix="all_"				      # Préfixe des fichiers de résultat (concaténés)
-pattern=SW*txt					          # Pattern pour sélectionner tous les fichiers input
+input=`pwd`                        # Path des fichiers input (pwd = répertoire courant)
+output="/home/gobertj1a/tmp"       # Path pour les fichiers/répertoires output
+output_prefix="all_"               # Préfixe des fichiers de résultat (concaténés)
+pattern=SW*txt                     # Pattern pour sélectionner tous les fichiers input
 
 # Variables fixes
-file_count=$(cd $input | ls -lrth $pattern | wc -l)	# Nombre de fichiers à traiter (correspondant au pattern)
-counter=0					 	                                # Nombre de fichiers traités
-current_time=$(date "+%Y%m%d_%H%M%S") 			        # Timestamp
+file_count=$(cd $input | ls -lrth $pattern | wc -l)        # Nombre de fichiers à traiter (correspondant au pattern)
+counter=0                                                  # Nombre de fichiers traités
+current_time=$(date "+%Y%m%d_%H%M%S")                      # Timestamp
 
 # Codes couleur
 nc='\033[0m'
@@ -47,16 +47,16 @@ echo -e "${yellow}$file_count fichiers à traiter dans $output${nc}"
 # Pour chaque fichier matché par le pattern
 for f in $pattern
 do
-  counter=$((counter+1)) 		    # Compteur
-  file_name=$(basename "$f")		# Récupération du nom du fichier sans le path
-  extension="${file_name##*.}"	# Récupération de l'extension du fichier
-  file_name="${file_name%.*}"	 	# Récupération du nom du fichier sans l'extension
+  counter=$((counter+1))              # Compteur
+  file_name=$(basename "$f")          # Récupération du nom du fichier sans le path
+  extension="${file_name##*.}"        # Récupération de l'extension du fichier
+  file_name="${file_name%.*}"         # Récupération du nom du fichier sans l'extension
 
   echo -e "  ${cyan}$counter) Traitement de $file_name..${nc}"
   # Création du message d'annulation
   sed -e 's/:22A:NEWT/:22A:CANC/' $f > cls/canc/"${file_name}.canc.${current_time}"
   # Création du message de reconfirmation
-  sed -e '0,/:57A:/s//:TODELETE:/' -e '/:TODELETE:/d' -e 's/:56A:/:57A:/' $f > cls/newt/"${file_name}.newt.${current_time}" 
+  sed -e '0,/:57A:/s//:TODELETE:/' -e '/:TODELETE:/d' -e 's/:56A:/:57A:/' -e 's/\(:20:[a-zA-Z0-9]\{8\}\)/\1B/' $f > cls/newt/"${file_name}.newt.${current_time}"
 done
 
 # Concaténation des fichiers dans le répertoire /cls
